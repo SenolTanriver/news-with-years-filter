26ab510a87a5fde916698e9cbb889e7dfdd1fd38
Şenol Tanrıver <senoltanriver@gmail.com>
2023-01-03 12:13:12 +0300

    updated

---
+++ Content/Js/main.js
@@ -0,0 +1,315 @@
+const elements = [
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2020, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/chopper.png'],
+    },
+    {
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2021, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/enduro.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2019, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/naked.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+    {
+
+        "date": [new Date(2022, 11, 25)],
+        "source": "lorem",
+        "desc": "Lorem ipsum dolor sit.",
+        "image": ['Content/images/super.png'],
+    },
+]
+
+const template = document.querySelector('#cardTemp');
+
+for (const element of elements) {
+    const date = element.date[0];
+    const source = element.source;
+    const desc = element.desc;
+    const image = element.image[0];
+
+    const card = template.cloneNode(true);
+    card.querySelector('img').src = image;
+    card.querySelector('#date1').textContent = date;
+    card.querySelector('#source').textContent = source;
+    card.querySelector('#desc').textContent = desc;
+    card.style.display = 'block';
+
+    document.querySelector('#conteiners').appendChild(card);
+}
+
+const links = document.querySelectorAll('#yearList a');
+  for (const link of links) {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const year = event.target.id;
+      filterByYear(year);
+      highlightLink(event.target);
+    });
+  }
+
+  function filterByYear(year) {
+    const elements = document.querySelectorAll('#conteiners .card');
+    for (const element of elements) {
+      const date = new Date(element.querySelector('#date1').textContent);
+      if (date.getFullYear() === parseInt(year)) {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none';
+      }
+    }
+  }
+
+  function highlightLink(link) {
+    const links = document.querySelectorAll('#yearList a');
+    for (const l of links) {
+      l.style.opacity = '0.5';
+    }
+    link.style.opacity = '1';
+  }
